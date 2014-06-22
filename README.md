54b54fc251b82cde185e064a532fa8fc5bc5431e
Tero Hänninen <tero@hanninen.eu>
2014-06-22 05:36:46 +0200

    API 1.1 and Python3 updates

--- README.md
+++ README.md
@@ -11,25 +11,24 @@ License: Totally completely Public Domain. Do whatever you want with it.
 
 Why?
 ----
-Because you might not want your old conversations around for years and years. Of course they'll probably be cached in various other places (like Google), but
+Because you might not want your old conversations around for years and years. Of course they'll probably be cached in various other places, but
 at least this way the link to your account will be broken. It's not perfect privacy (or really, privacy at all) - but then again nothing on Twitter is. 
 
 Pre-reqs and Installation.
 --------------------------
 
 You'll need 
-* Python 2.x
-* Tweepy (http://tweepy.github.com/)
-* Access to SMTP-server running at localhost
+* Python 3.x
+* Twython (https://github.com/ryanmcgrath/twython/)
+* Access to SMTP-server running at localhost if you want the e-mail
 
 In order to run it, you need to:
 * Login to Twitter dev (https://dev.twitter.com/
 * Go to "My Apps" (https://dev.twitter.com/apps)
-* Create New App
-* Once done, go to the OAuth tab
-* Open config.ini and fill in the required OAuth strings
+* Create New App and fill in the stuff they want (you don't need callback URL)
+* Once done, go to the API keys tab
+* Open config.ini and fill in the API key and secret, the app will try to do Oauth to fill in the rest. You'll want to keep your config.ini somewhere safe.
 
-That's it - it's pretty basic OAuth Twitter app setup.
 
 Usage
 ------
@@ -48,10 +47,6 @@ optional arguments:
   -t              test run, don't actually delete anything.
   </pre>
   
- The -m switch sends the deleted tweets to your mailbox for archiving. 
+ The optional -m switch sends the deleted tweets to your mailbox for archiving. 
  Try it out with the -t test-switch first so you don't lose anything valuable
  
- Limitations
- ------------
- * Clients may not make more than 350 requests per hour, so if you have a lot of tweets to delete, you'll hit this. There's no error handling, sorry. The script will just probably terminate.
- * It returns total of 3200 tweets (and then iterates through those to check for old replies). I have no idea what happens if you have more.
--- deleteTweetHistory.py
+++ deleteTweetHistory.py
@@ -4,40 +4,92 @@
 
 import argparse
 import datetime
-import tweepy
-from tweepy import Cursor
+from twython import Twython
+from twython import TwythonError
+from twython import TwythonRateLimitError
 import configparser
 
-def doLogin(consumer_key, consumer_secret, access_token, access_token_secret):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)    
-    return tweepy.API(auth)
 
-def findTweets(session, vDate):
-    api = session
-    lTempTweets = []
-    # Get tweets - Twitter API only returns max 800 replies.
-    tweets = Cursor(api.user_timeline, count=3200, include_rts=True)
-    for tweet in tweets.items():
-        if tweet.text.startswith("@"):
-            # it"s a reply
-            if vDate >= tweet.created_at:
-                # it"s old enough
-                lTempTweets.append(tweet)
-    return lTempTweets
+def doTwitterAuth(api_key, api_secret):
+	az = Twython(api_key, api_secret)
+	auth = az.get_authentication_tokens()
+	print("Get PIN from: " + auth['auth_url'])
+	auth_pin = input('Enter the PIN: ')
+	final_tokens = az.get_authorized_tokens(auth_pin.rstrip('\n'))
+	oauth_token = final_tokens['oauth_token']
+	oauth_secret = final_tokens['oauth_token_secret']
+	# Write the tokens to config for future use
+	config = configparser.ConfigParser()
+	oauth = config['OAUTH']
+	oauth['API_KEY'] = api_key
+	oauth['API_SECRET'] = api_secret
+	oauth['OAUTH_TOKEN'] = oauth_token
+	oauth['OAUTH_SECRET'] = oauth_secret
+	with open('config.ini', 'w') as configfile:
+		config.write(configfile)
+	return(oauth_token, oauth_secret)
 
-def doDelete(session, lDestroy): 
-    api = session
-    for tweet in lDestroy:
-        api.destroy_status(tweet.id)
-    return
+def findTweets(twitter, vDate):
+	lTempTweets = []
+	max_loops = 10
+	max_tweets = 2000
+	next_max_id = None 
+	
+	# Page through max_loops x 200 tweets
+	for i in range(0,max_loops):
+		# Return if we get over max_tweets
+		if(max_tweets < len(lTempTweets)):
+			return (lTempTweets)
+		
+		# First time around get the 200 latest tweets
+		if(0 == i):
+			results = twitter.get_user_timeline(include_rts='false', count='200')
+		# after that get the next 200 from max_id
+		else:
+			results = twitter.get_user_timeline(include_rts='false', count='200', max_id=next_max_id)
+		
+		# loop through results        
+		for tweet in results:
+			# it's a reply
+			next_max_id = tweet['id_str']
+			if (tweet['in_reply_to_user_id'] != None):
+				tweet_date = datetime.datetime.strptime(tweet['created_at'],"%a %b %d %H:%M:%S %z %Y")
+				simple_date = datetime.datetime.date(tweet_date)
+				# it's older than specified date
+				if (vDate >= simple_date):
+					# save it
+					lTempTweets.append(tweet)
+					
+	return (lTempTweets)
+
+def doDelete(twitter, lDestroy): 
+	lTempDelTweets = []
+	# loop through tweets...
+	for tweet in lDestroy:
+		# check if the api allows for more tries..
+		try:
+			# Finally, try to delete something
+			try:
+				# but don't if -t flag is on..
+				if not vTest:
+					twitter.destroy_status(id = tweet['id'])
+					# save it..
+					lTempDelTweets.append(tweet)
+					# Catch exception and exit the loop	
+			except TwythonError as e:
+				print (e)
+				break
+		except TwythonRateLimitError:	
+			print ("No available Twitter API requests. Try again in 15 minutes. Exiting.\n")
+			break
+	return(lTempDelTweets)
 
 def doReport(lTweetList):
     vToday = datetime.datetime.today()
     vTempReport = "This report was generated: {}\n".format(vToday)
     vTempReport += "Following tweets were deleted\n"
     for tweet in lTweetList:
-        vTempReport += "\t{}\t{}\t{}\n".format(tweet.id, tweet.created_at, tweet.text.encode('utf-8'))
+        vTempReport += "\t{}\t{}\t{}\n".format(tweet['id_str'], tweet['created_at'], tweet['text'])
     return(vTempReport)
     
 def sendEmail(vEmailAddress, vMessageBody):    
@@ -64,31 +116,38 @@ if __name__ == "__main__":
     # Command line args
     parser = argparse.ArgumentParser(description="Deletes old @reply conversations from Twitter.")
     parser.add_argument("-m", metavar="foo@bar.com", type=str, help="send report to this e-mail address.")
-    parser.add_argument("-d", metavar="YYYYMMDD", type=str, help="delete tweets older than this date.")
+    parser.add_argument("date", metavar="YYYYMMDD", type=str, help="delete tweets older than this date.")
     parser.add_argument("-t", action="store_true", help="test run, don't actually delete anything.")
     args = parser.parse_args()
 
-    vTargetDate = datetime.datetime.strptime(args.d,"%Y%m%d")
+    vTempDate = datetime.datetime.strptime(args.date,"%Y%m%d")
+    vTargetDate = datetime.datetime.date(vTempDate)
     vEmail = args.m
     vTest = args.t
     lDeleteTweets = []
     
     # Read config.ini for OAUTH strings
-    config = ConfigParser.RawConfigParser()
+    config = configparser.ConfigParser()
     config.read("config.ini")
-    CONSUMER_KEY = config.get("OAUTH", "CONSUMER_KEY")
-    CONSUMER_SECRET = config.get("OAUTH", "CONSUMER_SECRET")
+    API_KEY = config.get("OAUTH", "API_KEY")
+    API_SECRET = config.get("OAUTH", "API_SECRET")
+    if not (config['OAUTH']['OAUTH_TOKEN'] or config['OAUTH']['OAUTH_SECRET']):
+    	OAUTH_TOKEN, OAUTH_SECRET = doTwitterAuth(API_KEY, API_SECRET)
+    else:
+    	OAUTH_TOKEN = config.get("OAUTH", "OAUTH_TOKEN")
+    	OAUTH_SECRET = config.get("OAUTH", "OAUTH_SECRET")
     
-    # Authenticate session
-    session = doLogin(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    # Return tweet ID*s for deleteable tweets
-    lDeleteTweets = findTweets(session, vTargetDate)
-    # Don't delete if -t is defined
-    if not vTest:
-        doDelete(session,lDeleteTweets)
+    twitter = Twython(API_KEY, API_SECRET, OAUTH_TOKEN, OAUTH_SECRET)
+    status = twitter.verify_credentials()
+    
+    # Return deletable tweets
+    lDeleteTweets = findTweets(twitter, vTargetDate)
+    # Return deleted tweets
+    lNukedTweets = doDelete(twitter,lDeleteTweets)
     # Build the report body
-    vReport = doReport(lDeleteTweets)
-    print vReport
+    vReport = doReport(lNukedTweets)
+    print (vReport)
     # And e-mail it
-    sendEmail(vEmail,vReport)
+    if vEmail:
+    	sendEmail(vEmail,vReport)
     
